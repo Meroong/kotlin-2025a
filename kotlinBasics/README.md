549db6d12a163d5d3665064a276c99635c9a5d2c
merom <sonjoo1@dongyang.ac.kr>
2025-09-12 16:01:42 +0900

    week02 : README.md & .gitignore v0.1

---
+++ kotlinBasics/README.md
@@ -0,0 +1,11 @@
+#Week02 - Kotlin Basics 1
+
+## Learning Objectives
+- Understand basic Kotlin syntax
+- Decales variables and function
+
+## Practice Files
+- `MainActivity.kt`: variables, functions
+
+## How to Run
+click run button in android Studio.  (Shift + f10)
---
+++ kotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -0,0 +1,90 @@
+package com.kotlinbasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.kotlinbasics.ui.theme.KotlinBasicsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            KotlinBasicsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+        week02Functions()
+        week02Variable()
+    }
+}
+private fun week02Functions(){
+    //println("Week 02: Functions")
+
+    //fun greet(name: String) = "Hello, $name!"
+
+    //println(greet("Android developer"))
+
+    println("== Kotlin Functions ==")
+
+    fun greet(name: String): String {
+        return "Hello, $name!"
+    }
+    fun add(a: Int, b: Int) = a+b
+
+    fun introduce(name: String, age: Int = 19){
+        println("my name is $name and I'm $age years old")
+    }
+    println(greet("Kotlin"))
+    println("Sum: ${add(5, -71)}")
+    introduce("kim",  7)
+    introduce("Park", 19)
+}
+
+private fun week02Variable(){
+    println("Week02: Variables")
+    var courseName = "Mobile Programming"
+    //courseName = "Iot Programming"
+    var week =2
+    println("Course: $courseName")
+    println("Week : $week")
+
+    val age: Int = 24
+    val height: Double = 177.7
+    val isStudent: Boolean = false
+    println("Age: $age, Height $height, Student: $isStudent")
+
+    //var nickname: String = null
+    var nickname: String? = null
+    nickname = "mirae"
+    println("Nickname: $nickname ${nickname?.length}")
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    KotlinBasicsTheme {
+        Greeting("Android")
+    }
+}
